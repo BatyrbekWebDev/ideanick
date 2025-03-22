40c5160b7ffe6ec95243e065985e725ab9941343
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2025-03-23 00:23:36 +0300

    add trpc and tanstack

---
+++ backend/src/trpc.ts
@@ -0,0 +1,45 @@
+import { initTRPC } from "@trpc/server";
+
+const ideas = [
+  {
+    nick: "cool-idea-nick1",
+    name: "Idea 1",
+    description: "Description of idea 1",
+  },
+  {
+    nick: "cool-idea-nick2",
+    name: "Idea 2",
+    description: "Description of idea 2",
+  },
+  {
+    nick: "cool-idea-nick3",
+    name: "Idea 3",
+    description: "Description of idea 3",
+  },
+  {
+    nick: "cool-idea-nick4",
+    name: "Idea 4",
+    description: "Description of idea 4",
+  },
+  {
+    nick: "cool-idea-nick5",
+    name: "Idea 5",
+    description: "Description of idea 5",
+  },
+  {
+    nick: "cool-idea-nick6",
+    name: "Idea 6",
+    description: "Description of idea 6",
+  },
+];
+
+const trpc = initTRPC.create();
+
+export const trpcRouter = trpc.router({
+  getIdeas: trpc.procedure.query(() => {
+    return { ideas };
+  }),
+});
+
+
+export type TrpcRouter = typeof trpcRouter;
