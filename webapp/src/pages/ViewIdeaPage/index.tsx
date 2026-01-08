90ddb5f6b5cf6d0450a80212a06c4a09a1911957
Batyrbek <kasimov.batyr@gmail.com>
2026-01-08 20:19:18 +0300

    lesson 24

--- webapp/src/pages/ViewIdeaPage/index.tsx
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -1,17 +1,33 @@
-import { useParams } from 'react-router-dom'
-import { type ViewIdeaRouteParams } from '../../lib/routes'
+import { useParams } from "react-router-dom";
+import { type ViewIdeaRouteParams } from "../../lib/routes";
+import { trpc } from "../../lib/trpc";
 
 export const ViewIdeaPage = () => {
-  const { ideaNick } = useParams() as ViewIdeaRouteParams
+  const { ideaNick } = useParams() as ViewIdeaRouteParams;
+
+  const { data, error, isLoading, isFetching, isError } = trpc.getIdea.useQuery(
+    {
+      ideaNick,
+    }
+  );
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  if (!data.idea) {
+    return <span>idea not found</span>;
+  }
+
   return (
     <div>
-      <h1>{ideaNick}</h1>
-      <p>Description of the idea 1</p>
-      <div>
-        <p>paragraph 1</p>
-        <p>paragraph 2</p>
-        <p>paragraph 3</p>
-      </div>
+      <h1>{data.idea.name}</h1>
+      <p>{data.idea.description}</p>
+      <div dangerouslySetInnerHTML={{ __html: data.idea.text }} />
     </div>
   );
 };
