2c7a10f7db93788e0a23de926654fa8d68379efc
Batyrbek <kasimov.batyr@gmail.com>
2026-01-11 15:54:51 +0300

    lesson 28

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { trpc } from "../../lib/trpc";
 import { getViewIdeaRoute } from "../../lib/routes";
 import css from "./index.module.scss";
+import { Segment } from "../../components/Segment";
 
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError } =
@@ -16,23 +17,25 @@ export const AllIdeasPage = () => {
   }
 
   return (
-    <div>
-      <h1 className={css.title}>All Ideas</h1>
+    <Segment title="All ideas">
       <div className={css.ideas}>
         {data.ideas.map((idea) => (
           <div className={css.idea} key={idea.nick}>
-            <h2 className={css.ideaName}>
-              <Link
-                className={css.ideaLink}
-                to={getViewIdeaRoute({ ideaNick: idea.nick })}
-              >
-                {idea.name}
-              </Link>
-            </h2>
-            <p className={css.ideaDescription}>{idea.description}</p>
+            <Segment
+              size={2}
+              description={idea.description}
+              title={
+                <Link
+                  className={css.ideaLink}
+                  to={getViewIdeaRoute({ ideaNick: idea.nick })}
+                >
+                  {idea.name}
+                </Link>
+              }
+            ></Segment>
           </div>
         ))}
       </div>
-    </div>
+    </Segment>
   );
 };
